d85403ffabe5190d7dd8043ecee0c32ca4bdff2c
youssef yasser <youssifyasserme1999@gmail.com>
2023-01-13 04:21:09 +0200

    Head tag logo for all App

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,15 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Header from '../components/Header';
 import { SessionProvider } from 'next-auth/react';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
+      <Head>
+        <title>Medium Blog</title>
+        <link rel="icon" href="/favicon.png" />
+      </Head>
       <Header />
       <Component {...pageProps} />
     </SessionProvider>
